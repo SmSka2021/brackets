63bfeb76c79dedce730e9ee55d1a77a79f8571c7
SmSka2021 <91590651+SmSka2021@users.noreply.github.com>
2022-01-08 18:56:55 +0300

    i am the best

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+  let arr = bracketsConfig.flat(Infinity);
+  console.log(arr);
+  let set = new Set();
+  for (let i = 0; i < arr.length; i += 2) {
+    let a = arr[i] + arr[i + 1];
+    set.add(a);
+  }
+
+  let flag = true;
+  while (flag) {
+    flag = false;
+    for (let i = 0; i < str.length; i++) {
+      let isk = str[i] + str[i + 1];
+      if (set.has(isk)) {
+        str = str.replace(isk, "");
+        i++;
+        flag = true;
+      }
+    }
+  }
+  return !Boolean(str);
+};
